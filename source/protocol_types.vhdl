7990e55a70878d59b190af5d3785fe70ae3e237e
Julius Rakow <julius@familie-rakow.de>
2017-07-29 12:04:56 +0200

    add basic types and conversion functions

---
+++ source/protocol_types.vhdl
@@ -0,0 +1,176 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package protocol_types is
+	subtype t_ipv6_addr           is std_ulogic_vector(127 downto 0);
+	subtype t_ipv4_addr           is std_ulogic_vector( 31 downto 0);
+	subtype t_traffic_class       is std_ulogic_vector(  7 downto 0);
+	subtype t_next_header         is std_ulogic_vector(  7 downto 0);
+	subtype t_ipv6_flow_label     is std_ulogic_vector(19 downto 0);
+	subtype t_ipv4_identification is std_ulogic_vector(15 downto 0);
+	subtype t_ip_hop_limit        is std_ulogic_vector( 7 downto 0);
+	constant c_protocol_udp : t_next_header := x"11";
+	constant c_protocol_tcp : t_next_header := x"06";
+
+	subtype t_transport_port is std_ulogic_vector(15 downto 0);
+	subtype t_tcp_flags      is std_ulogic_vector( 7 downto 0);
+	subtype t_mac_addr       is std_ulogic_vector(47 downto 0);
+
+	subtype t_udp_checksum is std_ulogic_vector(15 downto 0);
+	type t_udp_header is record
+		source      : t_transport_port;
+		destination : t_transport_port;
+		length      : unsigned(15 downto 0);
+		checksum    : t_udp_checksum;
+	end record;
+	constant c_udp_header_width : natural := 64;
+	function to_udp_header(slv : std_ulogic_vector(c_udp_header_width - 1 downto 0)) return t_udp_header;
+	function to_std_ulogic_vector(uh : t_udp_header) return std_ulogic_vector;
+	constant c_udp_header_default : t_udp_header := (
+		source      => (others => '0'),
+		destination => (others => '0'),
+		length      => (others => '0'),
+		checksum    => (others => '0')
+	);
+
+	subtype t_ip_length is unsigned(15 downto 0);
+	type t_ipv6_header is record
+		version        : std_ulogic_vector(3 downto 0);
+		traffic_class  : t_traffic_class;
+		flow_label     : t_ipv6_flow_label;
+		payload_length : t_ip_length;
+		next_header    : t_next_header;
+		hop_limit      : t_ip_hop_limit;
+		source         : t_ipv6_addr;
+		destination    : t_ipv6_addr;
+	end record;
+	constant c_ipv6_header_width : natural := 320;
+	function to_ipv6_header(slv : std_ulogic_vector(c_ipv6_header_width - 1 downto 0)) return t_ipv6_header;
+	function to_std_ulogic_vector(ih : t_ipv6_header) return std_ulogic_vector;
+	constant c_ipv6_header_default : t_ipv6_header := (
+		version        => x"6",
+		traffic_class  => (others => '0'),
+		flow_label     => (others => '0'),
+		payload_length => (others => '0'),
+		next_header    => (others => '0'),
+		hop_limit      => (others => '0'),
+		source         => (others => '0'),
+		destination    => (others => '0')
+	);
+
+	type t_ipv4_header is record
+		version         : std_ulogic_vector(3 downto 0);
+		ihl             : std_ulogic_vector(3 downto 0);
+		traffic_class   : t_traffic_class;
+		total_length    : t_ip_length;
+		identification  : t_ipv4_identification;
+		flags           : std_ulogic_vector( 2 downto 0);
+		fragment_offset : std_ulogic_vector(12 downto 0);
+		time_to_live    : t_ip_hop_limit;
+		protocol        : t_next_header;
+		header_checksum : std_ulogic_vector(15 downto 0);
+		source          : t_ipv4_addr;
+		destination     : t_ipv4_addr;
+	end record;
+	constant c_ipv4_header_width : natural := 160;
+	function to_ipv4_header(slv : std_ulogic_vector(c_ipv4_header_width - 1 downto 0)) return t_ipv4_header;
+	function to_std_ulogic_vector(ih : t_ipv4_header) return std_ulogic_vector;
+	constant c_ipv4_header_default : t_ipv4_header := (
+		version         => x"4",
+		ihl             => x"5",
+		traffic_class   => (others => '0'),
+		total_length    => (others => '0'),
+		identification  => (others => '0'),
+		flags           => (others => '0'),
+		fragment_offset => (others => '0'),
+		time_to_live    => (others => '0'),
+		protocol        => (others => '0'),
+		header_checksum => (others => '0'),
+		source          => (others => '0'),
+		destination     => (others => '0')
+	);
+end package;
+
+package body protocol_types is
+	function to_udp_header(slv : std_ulogic_vector(c_udp_header_width - 1 downto 0)) return t_udp_header is
+		variable ret : t_udp_header := c_udp_header_default;
+	begin
+		ret.source      := slv(63 downto 48);
+		ret.destination := slv(47 downto 32);
+		ret.length      := unsigned(slv(31 downto 16));
+		ret.checksum    := slv(15 downto  0);
+		return ret;
+	end;
+	function to_std_ulogic_vector(uh : t_udp_header) return std_ulogic_vector is
+		variable ret : std_ulogic_vector(c_udp_header_width - 1 downto 0) := (others => '0');
+	begin
+		ret(63 downto 48) := uh.source     ;
+		ret(47 downto 32) := uh.destination;
+		ret(31 downto 16) := std_ulogic_vector(uh.length);
+		ret(15 downto  0) := uh.checksum   ;
+		return ret;
+	end;
+
+	function to_ipv6_header(slv : std_ulogic_vector(c_ipv6_header_width - 1 downto 0)) return t_ipv6_header is
+		variable ret : t_ipv6_header := c_ipv6_header_default;
+	begin
+		ret.version        := slv(319 downto 316);
+		ret.traffic_class  := slv(315 downto 308);
+		ret.flow_label     := slv(307 downto 288);
+		ret.payload_length := unsigned(slv(287 downto 272));
+		ret.next_header    := slv(271 downto 264);
+		ret.hop_limit      := slv(263 downto 256);
+		ret.source         := slv(255 downto 128);
+		ret.destination    := slv(127 downto   0);
+		return ret;
+	end;
+	function to_std_ulogic_vector(ih : t_ipv6_header) return std_ulogic_vector is
+		variable ret : std_ulogic_vector(c_ipv6_header_width - 1 downto 0) := (others => '0');
+	begin
+		ret(319 downto 316) := ih.version       ;
+		ret(315 downto 308) := ih.traffic_class ;
+		ret(307 downto 288) := ih.flow_label    ;
+		ret(287 downto 272) := std_ulogic_vector(ih.payload_length);
+		ret(271 downto 264) := ih.next_header   ;
+		ret(263 downto 256) := ih.hop_limit     ;
+		ret(255 downto 128) := ih.source        ;
+		ret(127 downto   0) := ih.destination   ;
+		return ret;
+	end;
+
+	function to_ipv4_header(slv : std_ulogic_vector(c_ipv4_header_width - 1 downto 0)) return t_ipv4_header is
+		variable ret : t_ipv4_header := c_ipv4_header_default;
+	begin
+		ret.version         := slv(159 downto 156);
+		ret.ihl             := slv(155 downto 152);
+		ret.traffic_class   := slv(151 downto 144);
+		ret.total_length    := unsigned(slv(143 downto 128));
+		ret.identification  := slv(127 downto 112);
+		ret.flags           := slv(111 downto 109);
+		ret.fragment_offset := slv(108 downto  96);
+		ret.time_to_live    := slv( 95 downto  88);
+		ret.protocol        := slv( 87 downto  80);
+		ret.header_checksum := slv( 79 downto  64);
+		ret.source          := slv( 63 downto  32);
+		ret.destination     := slv( 31 downto   0);
+		return ret;
+	end;
+	function to_std_ulogic_vector(ih : t_ipv4_header) return std_ulogic_vector is
+		variable ret : std_ulogic_vector(c_ipv4_header_width - 1 downto 0) := (others => '0');
+	begin
+		ret(159 downto 156) := ih.version        ;
+		ret(155 downto 152) := ih.ihl            ;
+		ret(151 downto 144) := ih.traffic_class  ;
+		ret(143 downto 128) := std_ulogic_vector(ih.total_length);
+		ret(127 downto 112) := ih.identification ;
+		ret(111 downto 109) := ih.flags          ;
+		ret(108 downto  96) := ih.fragment_offset;
+		ret( 95 downto  88) := ih.time_to_live   ;
+		ret( 87 downto  80) := ih.protocol       ;
+		ret( 79 downto  64) := ih.header_checksum;
+		ret( 63 downto  32) := ih.source         ;
+		ret( 31 downto   0) := ih.destination    ;
+		return ret;
+	end;
+end package body;
---
+++ source/record_types.vhdl
@@ -0,0 +1,235 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library ipfix_exporter;
+use ipfix_exporter.protocol_types.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package record_types is
+	--! AXI stream interface for packets master port
+	type t_if_axis_packet_m is record
+		tvalid : std_ulogic;
+		tdata  : std_ulogic_vector(127 downto 0);
+		tkeep  : std_ulogic_vector(15  downto 0);
+		tlast  : std_ulogic;
+	end record;
+	constant c_if_axis_packet_m_default : t_if_axis_packet_m := (
+		tvalid => '0',
+		tdata  => (others => '0'),
+		tkeep  => (others => '0'),
+		tlast  => '0'
+	);
+
+	--! common AXI stream slave interface
+	type t_if_axis_s is record
+		tready : std_ulogic;
+	end record;
+	constant c_if_axis_s_default : t_if_axis_s := (
+		tready => '0'
+	);
+
+	subtype t_timeout is unsigned(15 downto 0);
+
+	subtype t_timestamp    is unsigned(31 downto 0);
+	subtype t_packet_count is unsigned(31 downto 0);
+	subtype t_octet_count  is unsigned(31 downto 0);
+
+	/**
+	 * IPFIX data record with information about an IPv6 flow
+	 *
+	 * | element ID | length | name                     |
+	 * | ---------: | -----: | ------------------------ |
+	 * |         27 |     16 | sourceIPv6Address        |
+	 * |         28 |     16 | destinationIPv6Address   |
+	 * |          7 |      2 | sourceTransportPort      |
+	 * |         11 |      2 | destinationTransportPort |
+	 * |        150 |      4 | flowStartSeconds         |
+	 * |        151 |      4 | flowEndSeconds           |
+	 * |          1 |      4 | octetDeltaCount          |
+	 * |          2 |      4 | packetDeltaCount         |
+	 * |          4 |      1 | protocolIdentifier       |
+	 * |          5 |      1 | ipClassOfService         |
+	 * |          6 |      1 | tcpControlBits           |
+	 * |        210 |      9 | paddingOctets            |
+	 * |            |     64 | *total*                  |
+	 */
+	type t_ipfix_ipv6_data_record is record
+		src_ip_addr   : t_ipv6_addr;
+		dest_ip_addr  : t_ipv6_addr;
+		src_port      : t_transport_port;
+		dest_port     : t_transport_port;
+		start_time    : t_timestamp;
+		end_time      : t_timestamp;
+		octet_count   : t_octet_count;
+		packet_count  : t_packet_count;
+		next_header   : t_next_header;
+		traffic_class : t_traffic_class;
+		tcp_flags     : t_tcp_flags;
+		padding       : std_ulogic_vector(71 downto 0);
+	end record;
+	constant c_ipfix_ipv6_data_record_width : natural := 2 * 128 + 2 * 16 + 2 * 32 + 2 * 32 + 8 + 8 + 8 + 72;
+	function to_std_ulogic_vector(dr : t_ipfix_ipv6_data_record) return std_ulogic_vector;
+	function to_ipfix_ipv6_data_record(slv : std_ulogic_vector(c_ipfix_ipv6_data_record_width - 1 downto 0)) return t_ipfix_ipv6_data_record;
+	constant c_ipfix_ipv6_data_record_default : t_ipfix_ipv6_data_record := (
+		src_ip_addr   => (others => '0'),
+		dest_ip_addr  => (others => '0'),
+		src_port      => (others => '0'),
+		dest_port     => (others => '0'),
+		start_time    => (others => '0'),
+		end_time      => (others => '0'),
+		octet_count   => (others => '0'),
+		packet_count  => (others => '0'),
+		next_header   => (others => '0'),
+		traffic_class => (others => '0'),
+		tcp_flags     => (others => '0'),
+		padding       => (others => '0')
+	);
+
+	/**
+	 * IPFIX data record with information about an IPv4 flow
+	 *
+	 * | element ID | length | name                     |
+	 * | ---------: | -----: | ------------------------ |
+	 * |          8 |      4 | sourceIPv4Address        |
+	 * |         12 |      4 | destinationIPv4Address   |
+	 * |          7 |      2 | sourceTransportPort      |
+	 * |         11 |      2 | destinationTransportPort |
+	 * |        150 |      4 | flowStartSeconds         |
+	 * |        151 |      4 | flowEndSeconds           |
+	 * |          1 |      4 | octetDeltaCount          |
+	 * |          2 |      4 | packetDeltaCount         |
+	 * |          4 |      1 | protocolIdentifier       |
+	 * |          5 |      1 | ipClassOfService         |
+	 * |          6 |      1 | tcpControlBits           |
+	 * |        210 |      1 | paddingOctets            |
+	 * |            |     32 | *total*                  |
+	*/
+	type t_ipfix_ipv4_data_record is record
+		src_ip_addr   : t_ipv4_addr;
+		dest_ip_addr  : t_ipv4_addr;
+		src_port      : t_transport_port;
+		dest_port     : t_transport_port;
+		start_time    : t_timestamp;
+		end_time      : t_timestamp;
+		octet_count   : t_octet_count;
+		packet_count  : t_packet_count;
+		next_header   : t_next_header;
+		traffic_class : t_traffic_class;
+		tcp_flags     : t_tcp_flags;
+		padding       : std_ulogic_vector(7 downto 0);
+	end record;
+	constant c_ipfix_ipv4_data_record_width   : natural := 2 * 32 + 2 * 16 + 2 * 32 + 2 * 32 + 8 + 8 + 8 + 8;
+	function to_std_ulogic_vector(v4 : t_ipfix_ipv4_data_record) return std_ulogic_vector;
+	function to_ipfix_ipv4_data_record(slv : std_ulogic_vector(c_ipfix_ipv4_data_record_width - 1 downto 0)) return t_ipfix_ipv4_data_record;
+	constant c_ipfix_ipv4_data_record_default : t_ipfix_ipv4_data_record := (
+		src_ip_addr   => (others => '0'),
+		dest_ip_addr  => (others => '0'),
+		src_port      => (others => '0'),
+		dest_port     => (others => '0'),
+		next_header   => (others => '0'),
+		start_time    => (others => '0'),
+		end_time      => (others => '0'),
+		packet_count  => (others => '0'),
+		octet_count   => (others => '0'),
+		tcp_flags     => (others => '0'),
+		traffic_class => (others => '0'),
+		padding       => (others => '0')
+	);
+
+	--! AXI stream master interface for single IPv6 record transactions
+	type t_if_axis_ipv6_record_m is record
+		tvalid : std_ulogic;
+		tdata  : t_ipfix_ipv6_data_record;
+	end record;
+	constant c_if_axis_ipv6_record_m_default : t_if_axis_ipv6_record_m := (
+		tvalid => '0',
+		tdata  => c_ipfix_ipv6_data_record_default
+	);
+
+	--! AXI stream master interface for single IPv4 record transactions
+	type t_if_axis_ipv4_record_m is record
+		tvalid : std_ulogic;
+		tdata  : t_ipfix_ipv4_data_record;
+	end record;
+	constant c_if_axis_ipv4_record_m_default : t_if_axis_ipv4_record_m := (
+		tvalid => '0',
+		tdata  => c_ipfix_ipv4_data_record_default
+	);
+end package;
+
+package body record_types is
+	function to_std_ulogic_vector(dr : t_ipfix_ipv6_data_record) return std_ulogic_vector is
+		variable ret : std_ulogic_vector(c_ipfix_ipv6_data_record_width - 1 downto 0) := (others => '0');
+	begin
+		ret(511 downto 384) :=                   dr.src_ip_addr  ;
+		ret(383 downto 256) :=                   dr.dest_ip_addr ;
+		ret(255 downto 240) :=                   dr.src_port     ;
+		ret(239 downto 224) :=                   dr.dest_port    ;
+		ret(223 downto 192) := std_ulogic_vector(dr.start_time  );
+		ret(191 downto 160) := std_ulogic_vector(dr.end_time    );
+		ret(159 downto 128) := std_ulogic_vector(dr.octet_count );
+		ret(127 downto  96) := std_ulogic_vector(dr.packet_count);
+		ret( 95 downto  88) :=                   dr.next_header  ;
+		ret( 87 downto  80) :=                   dr.traffic_class;
+		ret( 79 downto  72) :=                   dr.tcp_flags    ;
+		ret( 71 downto   0) :=                   (others => '0') ;
+		return ret;
+	end;
+
+	function to_ipfix_ipv6_data_record(slv : std_ulogic_vector(c_ipfix_ipv6_data_record_width - 1 downto 0)) return t_ipfix_ipv6_data_record is
+		variable ret : t_ipfix_ipv6_data_record := c_ipfix_ipv6_data_record_default;
+	begin
+		ret.src_ip_addr   :=          slv(511 downto 384);
+		ret.dest_ip_addr  :=          slv(383 downto 256);
+		ret.src_port      :=          slv(255 downto 240);
+		ret.dest_port     :=          slv(239 downto 224);
+		ret.start_time    := unsigned(slv(223 downto 192));
+		ret.end_time      := unsigned(slv(191 downto 160));
+		ret.octet_count   := unsigned(slv(159 downto 128));
+		ret.packet_count  := unsigned(slv(127 downto  96));
+		ret.next_header   :=          slv( 95 downto  88);
+		ret.traffic_class :=          slv( 87 downto  80);
+		ret.tcp_flags     :=          slv( 79 downto  72);
+		ret.padding       :=          slv( 71 downto   0);
+		return ret;
+	end;
+
+	function to_std_ulogic_vector(v4 : t_ipfix_ipv4_data_record) return std_ulogic_vector is
+		variable ret : std_ulogic_vector(c_ipfix_ipv4_data_record_width - 1 downto 0) := (others => '0');
+	begin
+		ret(255 downto 224) :=                   v4.src_ip_addr  ;
+		ret(223 downto 192) :=                   v4.dest_ip_addr ;
+		ret(191 downto 176) :=                   v4.src_port     ;
+		ret(175 downto 160) :=                   v4.dest_port    ;
+		ret(159 downto 128) := std_ulogic_vector(v4.start_time   );
+		ret(127 downto  96) := std_ulogic_vector(v4.end_time     );
+		ret( 95 downto  64) := std_ulogic_vector(v4.octet_count  );
+		ret( 63 downto  32) := std_ulogic_vector(v4.packet_count );
+		ret( 31 downto  24) :=                   v4.next_header  ;
+		ret( 23 downto  16) :=                   v4.traffic_class;
+		ret( 15 downto   8) :=                   v4.tcp_flags    ;
+		ret(  7 downto   0) :=                   v4.padding      ;
+		return ret;
+	end;
+	function to_ipfix_ipv4_data_record(slv : std_ulogic_vector(c_ipfix_ipv4_data_record_width - 1 downto 0)) return t_ipfix_ipv4_data_record is
+		variable ret : t_ipfix_ipv4_data_record := c_ipfix_ipv4_data_record_default;
+	begin
+		ret.src_ip_addr   :=          slv(255 downto 224);
+		ret.dest_ip_addr  :=          slv(223 downto 192);
+		ret.src_port      :=          slv(191 downto 176);
+		ret.dest_port     :=          slv(175 downto 160);
+		ret.start_time    := unsigned(slv(159 downto 128));
+		ret.end_time      := unsigned(slv(127 downto  96));
+		ret.octet_count   := unsigned(slv( 95 downto  64));
+		ret.packet_count  := unsigned(slv( 63 downto  32));
+		ret.next_header   :=          slv( 31 downto  24);
+		ret.traffic_class :=          slv( 23 downto  16);
+		ret.tcp_flags     :=          slv( 15 downto   8);
+		ret.padding       :=          slv(  7 downto   0);
+		return ret;
+	end;
+end package body;
