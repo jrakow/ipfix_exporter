19f990edb760905bff23a16a6f153cac91ab7b1d
Julius Rakow <julius@familie-rakow.de>
2017-07-29 11:53:48 +0200

    add testbench and loopback module for test of testbench

---
+++ testbench.vhdl
@@ -0,0 +1,121 @@
+use std.env.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+library axis_testbench;
+
+/*!
+The testbench is a full environment to test a single module or combination of modules with AXI stream interfaces.
+
+The testbench instantiates an AXIS generator and an AXIS checker.
+
+The testbench generates a clock signal with a period specified by generic.
+For a single clock signal the global reset signal is set to active.
+The testbench then waites for the AXIS generator and checker modules.
+
+Also the test fails if a timeout is met.
+
+One AXIS interface connects the generator to the design under test input, another the output to the checker.
+ */
+entity testbench is
+	generic (
+		in_filename  : string := "cases/testbench_in.dat";
+		out_filename : string := "cases/testbench_out.dat";
+
+		g_in_tdata_width  : natural := 64;
+		g_out_tdata_width : natural := 64;
+
+		g_period  : time := 10 ns;
+		g_timeout : time :=  1 ms
+	);
+end entity;
+
+architecture arch of testbench is
+	signal s_clk : std_ulogic := '1';
+	signal s_rst : std_ulogic;
+
+	signal s_if_axis_in_m_tdata   : std_ulogic_vector(g_in_tdata_width - 1 downto 0);
+	signal s_if_axis_in_m_tvalid  : std_ulogic;
+	signal s_if_axis_in_s_tready  : std_ulogic;
+	signal s_if_axis_out_m_tdata  : std_ulogic_vector(g_out_tdata_width - 1 downto 0);
+	signal s_if_axis_out_m_tvalid : std_ulogic;
+	signal s_if_axis_out_s_tready : std_ulogic;
+
+	signal s_generator_finished : std_ulogic;
+	signal s_checker_finished   : std_ulogic;
+begin
+	s_clk <= not s_clk after g_period / 2;
+
+	p_testrun : process
+	begin
+		report "start";
+		s_rst <= '1';
+		wait until rising_edge(s_clk);
+		s_rst <= '0';
+
+		wait until rising_edge(s_clk) and s_generator_finished = '1';
+		report "generator finished";
+		wait until rising_edge(s_clk) and s_checker_finished = '1';
+		report "checker finished";
+		report "simulation finished";
+		-- exit without failure
+		stop(0);
+	end process;
+
+	i_axis_generator : entity axis_testbench.axis_generator
+		generic map(
+			g_filename    => in_filename,
+			g_tdata_width => g_in_tdata_width
+		)
+		port map(
+			clk              => s_clk,
+			rst              => s_rst,
+			if_axis_m_tdata  => s_if_axis_in_m_tdata,
+			if_axis_m_tvalid => s_if_axis_in_m_tvalid,
+			if_axis_s_tready => s_if_axis_in_s_tready,
+			finished         => s_generator_finished
+		);
+
+	i_axis_checker : entity axis_testbench.axis_checker
+		generic map(
+			g_filename    => out_filename,
+			g_tdata_width => g_out_tdata_width
+		)
+		port map(
+			clk              => s_clk,
+			rst              => s_rst,
+			if_axis_m_tdata  => s_if_axis_out_m_tdata,
+			if_axis_m_tvalid => s_if_axis_out_m_tvalid,
+			if_axis_s_tready => s_if_axis_out_s_tready,
+			finished         => s_checker_finished
+		);
+
+	i_design_under_test : entity axis_testbench.testbench_test_dummy
+		generic map(
+			g_in_tdata_width  => g_out_tdata_width,
+			g_out_tdata_width => g_out_tdata_width
+		)
+		port map(
+			clk                  => s_clk,
+			rst                  => s_rst,
+			if_axis_in_m_tdata   => s_if_axis_in_m_tdata,
+			if_axis_in_m_tvalid  => s_if_axis_in_m_tvalid,
+			if_axis_in_s_tready  => s_if_axis_in_s_tready,
+			if_axis_out_m_tdata  => s_if_axis_out_m_tdata,
+			if_axis_out_m_tvalid => s_if_axis_out_m_tvalid,
+			if_axis_out_s_tready => s_if_axis_out_s_tready
+		);
+
+	p_timeout : process
+	begin
+		report "timeout started";
+		wait for g_timeout;
+		assert false report "timeout reached" severity failure;
+		stop(1);
+	end process;
+end architecture;
---
+++ testbench_test_dummy.vhdl
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--! simple loopback entity to connect the testbench generator and checker directly together
+entity testbench_test_dummy is
+	generic(
+		g_in_tdata_width  : natural;
+		g_out_tdata_width : natural
+	);
+	port(
+		clk : in std_ulogic;
+		rst : in std_ulogic;
+
+		if_axis_in_m_tdata  : in  std_ulogic_vector(g_in_tdata_width - 1 downto 0);
+		if_axis_in_m_tvalid : in  std_ulogic;
+		if_axis_in_s_tready : out std_ulogic;
+
+		if_axis_out_m_tdata  : out std_ulogic_vector(g_out_tdata_width - 1 downto 0);
+		if_axis_out_m_tvalid : out std_ulogic;
+		if_axis_out_s_tready : in  std_ulogic
+	);
+end entity;
+
+architecture arch of testbench_test_dummy is
+begin
+	if_axis_out_m_tdata  <= if_axis_in_m_tdata;
+	if_axis_out_m_tvalid <= if_axis_in_m_tvalid;
+	if_axis_in_s_tready  <= if_axis_out_s_tready;
+
+	assert g_in_tdata_width = g_out_tdata_width
+		report "testbench_test_dummy may only used with the same tdata width"
+		severity failure;
+end architecture;
