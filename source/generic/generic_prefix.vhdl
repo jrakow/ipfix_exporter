04a5774be04d1f6cd7ea13a86e92fc0900ad6ce5
Julius Rakow <julius@familie-rakow.de>
2017-09-10 21:36:37 +0200

    extend generic prefix to accept longer prefixes

--- source/generic/generic_prefix.vhdl
+++ source/generic/generic_prefix.vhdl
@@ -8,7 +8,7 @@ use ipfix_exporter.pkg_types.all;
 
 /**
  * This module prefixes a stream with a given prefix.
- * The prefix is sampled at the first incoming handshake after an incoming handshake with tlast, i. e. the first transaction.
+ * The prefix must be held just as tdata on when tvalid is first set after tlast was set.
  */
 entity generic_prefix is
 	generic(
@@ -27,25 +27,32 @@ end entity;
 
 architecture arch of generic_prefix is
 	constant c_zeros : std_ulogic_vector(127 downto 0) := (others => '0');
-	type t_fsm is (receive, wait_read, send_last);
+	type t_fsm is (prefix_frames, receive, wait_read, send_last);
+
+	constant c_prefix_frames          : natural := g_prefix_width / 128;
+	constant c_prefix_remainder_width : natural := g_prefix_width mod 128;
 
 	type t_reg is record
-		fsm           : t_fsm;
-		prev_bytes    : std_ulogic_vector(g_prefix_width - 1 downto 0);
-		first_frame   : std_ulogic;
-		last_tkeep    : std_ulogic_vector(15 downto 0);
+		fsm                   : t_fsm;
+		prefix_frames_counter : natural range 0 to c_prefix_frames;
+		prefix_remainder      : std_ulogic_vector(c_prefix_remainder_width - 1 downto 0);
+		prev_bytes            : std_ulogic_vector(c_prefix_remainder_width - 1 downto 0);
+		first_frame           : std_ulogic;
+		last_tkeep            : std_ulogic_vector(15 downto 0);
 
 		-- output
 		if_axis_in_s  : t_if_axis_s;
 		if_axis_out_m : t_if_axis_frame_m;
 	end record;
 	constant c_reg_default : t_reg := (
-		fsm           => receive,
-		prev_bytes    => (others => '0'),
-		first_frame   => '1',
-		last_tkeep    => (others => '0'),
-		if_axis_in_s  => c_if_axis_s_default,
-		if_axis_out_m => c_if_axis_frame_m_default
+		fsm                   => prefix_frames,
+		prefix_frames_counter => 0,
+		prefix_remainder      => (others => '0'),
+		prev_bytes            => (others => '0'),
+		first_frame           => '1',
+		last_tkeep            => (others => '0'),
+		if_axis_in_s          => c_if_axis_s_default,
+		if_axis_out_m         => c_if_axis_frame_m_default
 	);
 	signal r, r_nxt        : t_reg := c_reg_default;
 begin
@@ -68,6 +75,36 @@ begin
 		v := r;
 
 		case r.fsm is
+			-- only used if the prefix is more than one frame
+			when prefix_frames =>
+				v.if_axis_in_s.tready := '0';
+				if c_prefix_frames = 0 then
+					-- this state is ignored
+					v.fsm := receive;
+				elsif if_axis_in_m.tvalid then
+					-- master starts sending
+					v.if_axis_out_m.tvalid := '1';
+					v.if_axis_out_m.tkeep  := (others => '1');
+					v.if_axis_out_m.tlast  := '0';
+
+					-- sample prefix frames containing in data
+					v.prefix_remainder := prefix(c_prefix_remainder_width - 1 downto 0);
+
+					if if_axis_out_m.tvalid and if_axis_out_s.tready then
+						v.prefix_frames_counter := v.prefix_frames_counter + 1;
+						if v.prefix_frames_counter = c_prefix_frames then
+							-- reset counter
+							v.prefix_frames_counter := c_reg_default.prefix_frames_counter;
+
+							v.if_axis_out_m.tvalid := '0';
+							v.if_axis_in_s.tready  := '1';
+							v.fsm := receive;
+						end if;
+					end if;
+
+					v.if_axis_out_m.tdata  := prefix(g_prefix_width - v.prefix_frames_counter * 128 - 1 downto g_prefix_width - (v.prefix_frames_counter + 1) * 128);
+				end if;
+
 			when receive =>
 				v.if_axis_in_s.tready := '1';
 
@@ -75,12 +112,12 @@ begin
 					v.if_axis_in_s.tready := '0';
 
 					if v.first_frame then
-						v.if_axis_out_m.tdata := prefix & if_axis_in_m.tdata(127 downto g_prefix_width);
+						v.if_axis_out_m.tdata := v.prefix_remainder(c_prefix_remainder_width - 1 downto 0) & if_axis_in_m.tdata(127 downto c_prefix_remainder_width);
 						v.first_frame         := '0';
 					else
-						v.if_axis_out_m.tdata := v.prev_bytes & if_axis_in_m.tdata(127 downto g_prefix_width);
+						v.if_axis_out_m.tdata := v.prev_bytes & if_axis_in_m.tdata(127 downto c_prefix_remainder_width);
 					end if;
-					v.prev_bytes := if_axis_in_m.tdata(g_prefix_width - 1 downto 0);
+					v.prev_bytes := if_axis_in_m.tdata(c_prefix_remainder_width - 1 downto 0);
 
 					-- defaults
 					v.if_axis_out_m.tvalid := '1';
@@ -89,15 +126,15 @@ begin
 					v.fsm                  := wait_read;
 
 					if if_axis_in_m.tlast then
-						if tkeep_to_integer(if_axis_in_m.tkeep) + g_prefix_width / 8 <= 16 then
+						if tkeep_to_integer(if_axis_in_m.tkeep) + c_prefix_remainder_width / 8 <= 16 then
 							-- prefix and incoming fit into single frame
-							v.if_axis_out_m.tkeep := to_tkeep(tkeep_to_integer(if_axis_in_m.tkeep) + g_prefix_width / 8, 16);
+							v.if_axis_out_m.tkeep := to_tkeep(tkeep_to_integer(if_axis_in_m.tkeep) + c_prefix_remainder_width / 8, 16);
 							v.if_axis_out_m.tlast := '1';
 							v.first_frame         := '1';
 							v.fsm                 := wait_read;
 						else
 							-- full frame and one more
-							v.last_tkeep := to_tkeep(tkeep_to_integer(if_axis_in_m.tkeep) + g_prefix_width / 8 - 16, 16);
+							v.last_tkeep := to_tkeep(tkeep_to_integer(if_axis_in_m.tkeep) + c_prefix_remainder_width / 8 - 16, 16);
 							v.fsm        := send_last;
 						end if;
 					end if;
@@ -106,14 +143,18 @@ begin
 			when wait_read =>
 				if if_axis_out_m.tvalid and if_axis_out_s.tready then
 					v.if_axis_out_m.tvalid := '0';
-					v.if_axis_in_s.tready  := '1';
-					v.fsm                  := receive;
+					if if_axis_out_m.tlast then
+						v.fsm := prefix_frames;
+					else
+						v.if_axis_in_s.tready  := '1';
+						v.fsm := receive;
+					end if;
 				end if;
 
 			when send_last =>
 				if if_axis_out_m.tvalid and if_axis_out_s.tready then
 					v.if_axis_out_m.tkeep := v.last_tkeep;
-					v.if_axis_out_m.tdata := v.prev_bytes & c_zeros(127 - g_prefix_width downto 0);
+					v.if_axis_out_m.tdata := v.prev_bytes & c_zeros(127 - c_prefix_remainder_width downto 0);
 					v.if_axis_out_m.tlast := '1';
 
 					v.last_tkeep  := (others => '0');
