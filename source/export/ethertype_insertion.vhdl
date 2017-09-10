6c5baf5ad64332e9dcc28357d171c1261b302d7a
Julius Rakow <julius@familie-rakow.de>
2017-09-10 20:21:11 +0200

    implement ethertype insertion using generic prefix

--- source/export/ethertype_insertion.vhdl
+++ source/export/ethertype_insertion.vhdl
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 library ipfix_exporter;
 use ipfix_exporter.pkg_axi_stream.all;
+use ipfix_exporter.pkg_config.all;
 use ipfix_exporter.pkg_types.all;
 
 /*!
@@ -23,5 +24,26 @@ entity ethertype_insertion is
 end entity;
 
 architecture arch of ethertype_insertion is
+	constant c_prefix_width : natural := 16;
+	signal s_prefix : std_ulogic_vector(c_prefix_width - 1 downto 0);
 begin
+	-- ip version from ip header
+	s_prefix <= x"86DD" when if_axis_in_m.tdata(127 downto 124) = x"6" else x"0800";
+
+	i_generic_prefix : entity ipfix_exporter.generic_prefix
+		generic map(
+			g_prefix_width => c_prefix_width
+		)
+		port map(
+			clk           => clk,
+			rst           => rst,
+
+			if_axis_in_m  => if_axis_in_m,
+			if_axis_in_s  => if_axis_in_s,
+
+			if_axis_out_m => if_axis_out_m,
+			if_axis_out_s => if_axis_out_s,
+
+			prefix        => s_prefix
+		);
 end architecture;
